90c44f6ed6b92ab53690418f5feaf45f09b077f2
Francisco Dans <fdansv@gmail.com>
2014-09-29 19:44:14 +0200

    adds pachage shell script

---
+++ packageAndInstall.sh
@@ -0,0 +1,3 @@
+echo "Packaging and installing the plugin into ElasticSearch"
+mvn clean package
+/usr/local/Cellar/elasticsearch/1.2.1/bin/plugin -url file:./target/release/elasticsearch-PeliasPlugin-0.17.1.zip -install awesome-plugin
