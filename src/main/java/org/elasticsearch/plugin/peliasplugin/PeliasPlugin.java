2a5a89a899eff64b050897234d2e6e3034942c90
Francisco Dans <fdansv@gmail.com>
2014-09-30 15:11:22 +0200

    subclasses AbstractModule, adds Analyzer call

--- src/main/java/org/elasticsearch/plugin/peliasplugin/PeliasPlugin.java
+++ src/main/java/org/elasticsearch/plugin/peliasplugin/PeliasPlugin.java
@@ -1,13 +1,35 @@
 package org.elasticsearch.plugin.peliasplugin;
 
+import org.elasticsearch.common.inject.Module;
+import org.elasticsearch.index.analysis.AnalysisModule;
+import org.elasticsearch.index.analysis.PeliasAnalysisBinderProcessor;
+import org.elasticsearch.plugins.AbstractPlugin;
+
 /**
  * Hello world!
  *
  */
-public class PeliasPlugin
+public class PeliasPlugin extends AbstractPlugin
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    @Override
+    public String name() {
+        return "pelias-analysis";
+    }
+
+    @Override
+    public String description() {
+        return "Plugin that performs all analysis tasks in Pelias";
+    }
+
+    @Override public void processModule(Module module) {
+        if (module instanceof AnalysisModule) {
+            AnalysisModule analysisModule = (AnalysisModule) module;
+            analysisModule.addProcessor(new PeliasAnalysisBinderProcessor());
+        }
     }
 }
