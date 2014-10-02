31cbd97f5a6c590f7ecd0eeea4089016da2b2846
Francisco Dans <fdansv@gmail.com>
2014-10-02 18:51:45 +0200

    reflect ES version change

--- packageAndInstall.sh
+++ packageAndInstall.sh
@@ -1,3 +1,3 @@
 echo "Packaging and installing the plugin into ElasticSearch"
 mvn clean package
-/usr/local/Cellar/elasticsearch/1.2.1/bin/plugin -url file:./target/release/elasticsearch-PeliasPlugin-0.17.1.zip -install pelias-plugin
+/usr/local/Cellar/elasticsearch/1.2.1/bin/plugin -url file:./target/release/elasticsearch-PeliasPlugin-1.3.4.zip -install pelias-plugin
