393cadb7e96e072cd3e13dafcf565a6fb21b1723
Francisco Dans <fdansv@gmail.com>
2014-10-02 18:54:57 +0200

    comments out pluginservice temporarily

--- src/main/java/org/elasticsearch/PeliasPlugin/PeliasPluginService.java
+++ src/main/java/org/elasticsearch/PeliasPlugin/PeliasPluginService.java
@@ -1,30 +1,30 @@
-package org.elasticsearch.PeliasPlugin;
-
-import org.elasticsearch.ElasticSearchException;
-import org.elasticsearch.common.component.AbstractLifecycleComponent;
-import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.settings.SettingsFilter;
-
-public class PeliasPluginService extends AbstractLifecycleComponent<PeliasPluginService>{
-    @Inject
-    public PeliasPluginService(Settings settings, SettingsFilter settingsFilter){
-        super(settings);
-    }
-
-    @Override
-    protected void doStart() throws ElasticSearchException {
-        String host = componentSettings.get("host", "localhost");
-        int port = componentSettings.getAsInt("port", 9200);
-    }
-
-    @Override
-    protected void doStop() throws ElasticSearchException {
-
-    }
-
-    @Override
-    protected void doClose() throws ElasticSearchException {
-
-    }
-}
+//package org.elasticsearch.PeliasPlugin;
+//
+//import org.elasticsearch.ElasticSearchException;
+//import org.elasticsearch.common.component.AbstractLifecycleComponent;
+//import org.elasticsearch.common.inject.Inject;
+//import org.elasticsearch.common.settings.Settings;
+//import org.elasticsearch.common.settings.SettingsFilter;
+//
+//public class PeliasPluginService extends AbstractLifecycleComponent<PeliasPluginService>{
+//    @Inject
+//    public PeliasPluginService(Settings settings, SettingsFilter settingsFilter){
+//        super(settings);
+//    }
+//
+//    @Override
+//    protected void doStart() throws ElasticSearchException {
+//        String host = componentSettings.get("host", "localhost");
+//        int port = componentSettings.getAsInt("port", 9200);
+//    }
+//
+//    @Override
+//    protected void doStop() throws ElasticSearchException {
+//
+//    }
+//
+//    @Override
+//    protected void doClose() throws ElasticSearchException {
+//
+//    }
+//}
