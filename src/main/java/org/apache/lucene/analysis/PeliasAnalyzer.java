8e8e15b7f1108ae50cecfeba12e08dff4afdf700
Francisco Dans <fdansv@gmail.com>
2014-10-02 18:25:36 +0200

    replicates current analysis functionality in Java

--- src/main/java/org/apache/lucene/analysis/PeliasAnalyzer.java
+++ src/main/java/org/apache/lucene/analysis/PeliasAnalyzer.java
@@ -1,27 +1,40 @@
 package org.apache.lucene.analysis;
 
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+import org.apache.lucene.analysis.miscellaneous.WordDelimiterFilter;
+import org.apache.lucene.analysis.synonym.SynonymFilter;
+import org.apache.lucene.analysis.synonym.SynonymMap;
 import org.apache.lucene.util.Version;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.Reader;
 
+
 public class PeliasAnalyzer extends Analyzer {
-    private Boolean cacheTokenStreams;
+    private final Version version;
 
-    public PeliasAnalyzer(Version version, Analyzer... subAnalyzers) {
-
+    public PeliasAnalyzer(Version version) {
+        this.version = version;
     }
+
 
     @Override
-    protected TokenStreamComponents createComponents(String s, Reader reader) {
-        return null;
-    }
+    protected TokenStreamComponents createComponents(String fieldName,
+                                                     Reader reader) {
+        Tokenizer source = new WhitespaceTokenizer(version, reader);
+        TokenStream filter = new LowerCaseFilter(version, source);
+        //TODO AMPERSAND FILTER
+        filter = new WordDelimiterFilter(version, filter, 0, null);
+//        SynonymMap synonymMap = null;
+//        try {
+//            synonymMap = new SynonymMap(new FileInputStream("samples/fulltext/wn_s.pl"));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        filter = new SynonymFilter(filter, );
 
-    public PeliasAnalyzer setTokenStreamCachingEnabled(Boolean tokenstreamCaching) {
-        cacheTokenStreams = tokenstreamCaching;
-        return this;
-    }
-
-    public void setDeduplicationEnabled(Boolean deduplication) {
-
+        return new TokenStreamComponents(source, filter);
     }
 }
