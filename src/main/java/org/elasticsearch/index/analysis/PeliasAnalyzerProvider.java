7d9a3050fba24bddc4cdf839c313a2cd88e5a0f7
Francisco Dans <fdansv@gmail.com>
2014-10-02 17:29:45 +0200

    adds structure to provider

--- src/main/java/org/elasticsearch/index/analysis/PeliasAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/PeliasAnalyzerProvider.java
@@ -1,4 +1,20 @@
 package org.elasticsearch.index.analysis;
 
+import org.apache.lucene.analysis.Analyzer;
+
 public class PeliasAnalyzerProvider implements AnalyzerProvider {
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public AnalyzerScope scope() {
+        return null;
+    }
+
+    @Override
+    public Analyzer get() {
+        return null;
+    }
 }
