c8785ac9edee373f52d363273eb6089440926e95
Francisco Dans <fdansv@gmail.com>
2014-10-02 17:58:44 +0200

    adds data model for analyzer provider

--- src/main/java/org/elasticsearch/index/analysis/PeliasAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/PeliasAnalyzerProvider.java
@@ -1,20 +1,34 @@
 package org.elasticsearch.index.analysis;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.PeliasAnalyzerWrapper;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.inject.Injector;
+import org.elasticsearch.common.inject.assistedinject.Assisted;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.settings.IndexSettings;
 
-public class PeliasAnalyzerProvider implements AnalyzerProvider {
-    @Override
-    public String name() {
-        return null;
+public class PeliasAnalyzerProvider extends AbstractIndexAnalyzerProvider<PeliasAnalyzerWrapper> {
+
+    private final String name;
+    private final Injector injector;
+    private final Settings settings;
+
+    @Inject
+    PeliasAnalyzerProvider(Index index,
+                           @IndexSettings Settings indexSettings,
+                           @Assisted String name,
+                           @Assisted Settings settings,
+                           Injector injector){
+        super(index, indexSettings, name, settings);
+        this.injector = injector;
+        this.settings = settings;
+        this.name = name;
     }
 
     @Override
-    public AnalyzerScope scope() {
-        return null;
-    }
-
-    @Override
-    public Analyzer get() {
-        return null;
+    public PeliasAnalyzerWrapper get() {
+        return new PeliasAnalyzerWrapper(version, name, settings, injector);
     }
 }
